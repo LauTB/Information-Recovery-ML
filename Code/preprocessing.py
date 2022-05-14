98d30fb7f1e235ef396516d432e8cba7d541ef74
JessyGigato <jessygigato@gmail.com>
2022-05-13 23:51:22 -0700

    join

---
+++ Code/preprocessing.py
@@ -0,0 +1,46 @@
+import glob 
+import codecs
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+#import pandas as pd
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def preprocess(docs):
+    
+    proc_docs =  []
+    for doc in docs:
+        #get_tokens
+        tokens = nltk.word_tokenize(doc)
+        #removing stopwords
+        new_doc = remove_stopwords(tokens)
+        #stemming
+        ps = PorterStemmer()
+        stemmed = []
+        for word in tokens:
+            stemmed.append(ps.stem(word))
+        proc_docs.append(' '.join(stemmed))
+    return proc_docs
+
+
+def remove_stopwords(doc):
+    cleaned_text = []
+    stop_words = set(stopwords.words('english'))
+    for word in doc:
+        if word not in stop_words:
+            cleaned_text.append(word)
+    return cleaned_text
+
+def transform(proc_docs,query):
+    vectorizerX = TfidfVectorizer()
+    vectorizerX.fit(proc_docs)
+
+    doc_vector = vectorizerX.transform(proc_docs)
+    query_vector = vectorizerX.transform(query)
+
+    return doc_vector,query_vector
+
---
+++ Code/retrieval.py
@@ -0,0 +1,32 @@
+from preprocessing import preprocess, transform
+from vect_model import vectorial_model
+
+docs = [
+      'In computer science artificial intelligence sometimes called machine intelligence is intelligence demonstrated by machines',
+
+      'Experimentation calculation and Observation is called science',
+
+      'Physics is a natural science that involves the study of matter and its motion through space and time, along with related concepts such as energy and force',
+      
+      'In mathematics and computer science an algorithm is a finite sequence of well-defined computer-implementable instructions',
+
+      'Chemistry is the scientific discipline involved with elements and compounds composed of atoms, molecules and ions',
+
+      'Biochemistry is the branch of science that explores the chemical processes within and related to living organisms',
+
+      'Sociology is the study of society, patterns of social relationships, social interaction, and culture that surrounds everyday life',
+    
+    ]
+query = 'computer science'
+
+def retrieval(docs,query):
+    proc_docs = preprocess(docs)
+    proc_query = preprocess([query])
+    doc_vector,query_vector = transform(proc_docs,proc_query)
+    indexs = vectorial_model(doc_vector,query_vector)
+    result = [proc_docs[i] for i in indexs]
+    return result
+
+
+retrieval(docs,query)
+
